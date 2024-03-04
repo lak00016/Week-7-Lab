04e8ddf6f0df5732dc5277fb1f02bc57184052c5
lak00016 <161257339+lak00016@users.noreply.github.com>
2024-03-03 22:58:51 -0500

    completed tasks for assigmment
    added nav bar and cards

---
+++ src/Card.js
@@ -0,0 +1,29 @@
+function Card() {
+    return (
+<div className="d-flex justify-content-around">
+    <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+    <div class="card-header">Card 1</div>
+    <div class="card-body">
+    <h5 class="card-title">Card 1</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
+  <div class="card-header">Card 2</div>
+  <div class="card-body">
+    <h5 class="card-title">Card 2</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+  <div class="card-header">Card 3</div>
+  <div class="card-body">
+    <h5 class="card-title">Card 3</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+      </div>
+    );
+  }
+  
+  export default Card;
