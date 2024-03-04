1eacdd8e301286c3ea212a07bb1cedc6a4005b6e
lak00016 <161257339+lak00016@users.noreply.github.com>
2024-03-03 23:51:02 -0500

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Navbar from'./Navbar';
+import Navbar from './Navbar';
 import Card from './Card';
 
 function App() {
   return (
+    
     <div className="App">
+      <Navbar />
      <h1>CS 230L</h1>
      <h2>Section-002</h2>
      <p>WVU ID: 800401603</p>
      <p>Hi I am Luke</p>
+    
+     <Card />
     </div>
   );
 }
